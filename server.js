e762364654bc3d6e422ded9b7611be113098becc
Alex Imre <alexandre.imre@gmail.com>
2022-09-30 16:26:55 +0100

    added minions API functionality

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const cors = require('cors');
+const { createMeeting,
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+  deleteAllFromDatabase } = require('./server/db.js');
 
 module.exports = app;
 
@@ -16,9 +24,48 @@ app.use(cors());
 // Add middware for parsing request bodies here:
 app.use(bodyParser.json());
 
+// Logging
+app.use(morgan('short'));
+
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
 app.use('/api', apiRouter);
+
+// Get all minions
+apiRouter.get('/minions', (req, res, next) => {
+  const minions = getAllFromDatabase('minions');
+  res.status(200).send(minions);
+})
+
+// Create a minion
+apiRouter.post('/minions', (req, res, next) => {
+  // id assignment taken care of by DB??
+  const newMinion = req.body;
+  addToDatabase('minions', newMinion);
+  res.status(201).send(newMinion);
+})
+
+// Get minion by id
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+  const minionId = String(req.params.minionId);
+  console.log(`minion Id is: ${minionId}`);
+  const minion = getFromDatabaseById('minions', minionId);
+  res.status(200).send(minion);
+})
+
+// Update minion by id
+apiRouter.put('/minions/:minionId', (req, res, next) => {
+  const updatedMinion = req.body;
+  updateInstanceInDatabase('minions', updatedMinion);
+  res.status(200).send(updatedMinion);
+})
+
+// Delete a minion
+apiRouter.delete('/minions/:minionId', (req, res, next) => {
+  const minionId = String(req.params.minionId)
+  deleteFromDatabasebyId('minions', minionId);
+  res.status(204).send();
+})
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
